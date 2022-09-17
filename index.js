66c21360b6038923da3f622b2abf08619691c813
Rushikesh Shirsat <90494892+Rushikesh222@users.noreply.github.com>
2022-09-17 08:09:30 +0000

    changes has be done

--- index.js
+++ index.js
@@ -3,19 +3,16 @@ var score = 0;
 var userName = readLineSync.question("What is Your Name? ")
 console.log("Welcome " + userName + " Do You Know Rushikesh? ")
 console.log(userName + " If You Type Wrong Answer then You Will Get -1 And Right Answer Will Give You 1 Point ")
-var highScore=[{
-  name:"Rushikesh",
-  score:6,
-  
-},{
-  name:"Tejas",
-  score:5,
-  
-}]
+console.log(userName + " Important notice first letter is capital and some answer have space")
+var highScore = {
+  name: "Tejas",
+  score: 5,
+
+}
 
 function play(question, answer) {
   var userAnswer = readLineSync.question(question)
-  if (userAnswer.toUpperCase === answer.toUpperCase) {
+  if (userAnswer === answer) {
     console.log("Right")
     score = score + 1
   } else {
@@ -26,26 +23,31 @@ function play(question, answer) {
   console.log("-------")
 }
 var question = [{
-  question: "Which is My Favourite Movie? ",
+  question: "Which is My Favourite Movie in Avenger? ",
   answer: "Infinity War",
 }, {
   question: "Which is My Favourite Game? ",
   answer: "Cricket",
 }, {
   question: "Whick Anime I Like Most ? ",
-  answer: "Attack on titan",
+  answer: "Attack On Titan",
 }, {
   question: "Which is My Favourite Dish To Eat ? ",
-  answer: "chicken chilli",
+  answer: "Chicken Chilli",
 }, {
   question: "Which Bollywood Series I Most Like  ? ",
   answer: "Asur",
 }, {
   question: "Whick Thing Makes Me Happy? ",
-  answer: "great",
+  answer: "Paying Cricket",
 },]
 for (i = 0; i < question.length; i++) {
   var currentQuestion = question[i]
   play(currentQuestion.question, currentQuestion.answer)
+}
+if (highScore.score = score.value) {
+  console.log("you score higher than " + highScore.name + " My friend" + userName)
+} else {
+  console.log("you score less than " + highScore.name + " My friend " + userName)
 }
 console.log("YAY! You Scored", score)
